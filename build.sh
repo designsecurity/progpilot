69ca11fd0a8e67b5a8a226a27d462b21729073cc
eric <eric.therond.fr@gmail.com>
2017-08-28 18:38:18 +0200

    builds

--- build.sh
+++ build.sh
@@ -6,9 +6,14 @@ newfile="progpilot_${version}${date}"
     
 cd ./projects/phar
 rm composer.lock
-rm -R ./vendor/*
+rm -r ./vendor/*
 rmdir ./vendor
 composer install
+
+echo "testing ..."
+output_runall=""
+output_runallfolders=""
+output_runexcludefiles=""
 
 output_runall=`cd ../tests; php ./run_all.php`
 output_runallfolders=`cd ../tests; php ./run_all_folders.php`
@@ -34,6 +39,8 @@ else
         then rm $file
         fi
     done
+    
+    echo "generating phar"
 
     php -d phar.readonly=off ./phar-composer.phar build .
     mv ./rogpilot.phar ../../builds/${newfile}.phar
