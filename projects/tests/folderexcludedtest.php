766f4bf2ad7fcc3e8d2eb773c24aad45423a3c10
eric <eric.therond.fr@gmail.com>
2017-11-09 16:43:37 +0100

    test exclude_files

--- projects/tests/folderexcludedtest.php
+++ projects/tests/folderexcludedtest.php
@@ -15,8 +15,5 @@ $framework->add_testbasis("./tests/folders/folder2");
 $framework->add_output("./tests/folders/folder2", array("\$var1[0]"));
 $framework->add_output("./tests/folders/folder2", array("5"));
 $framework->add_output("./tests/folders/folder2", "xss");
-$framework->add_output("./tests/folders/folder2", array("\$var1"));
-$framework->add_output("./tests/folders/folder2", array("9"));
-$framework->add_output("./tests/folders/folder2", "xss");
 
 ?>
