7379475b218950859a9f75d1ed99ae9b2fa8af8e
Nicolas CARPi <nicolas.carpi@curie.fr>
2020-03-23 01:30:45 +0100

    Improve the build.sh script
    * User /usr/bin/env instead of hardcoded path for bash
    * Check if command is in $PATH instead of hardcoding it in the script
    * Exit early if the command is not found
    * Use modern $() syntax instead of backticks
    * Group the cleanup commands together
    * Merge the mkdir commands in one with -p flag
    * Add -f to rm composer.lock command so it doesn't complain if file is
      not here
    * Script is now passing shellcheck with no errors
    * Add verbose flag to final move
    * Add phar builds to .gitignore

--- build.sh
+++ build.sh
@@ -1,40 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-composertool=/home/eric/dev/phar-composer.phar
-version="dev"
-date=`date "+%Y%m%d-%H%M%S"`
-newfile="progpilot_${version}${date}"
-    
-echo "Progpilot builder"
-echo "Have you updated the version of progpilot in Console/Application.php file ? (optional)"
-
-if [ -e ${composertool} ] 
-then
-    cd ./projects/phar
-    rm composer.lock
-    rm -rf ./vendor/
-    composer install
-
-    rm -rf ./vendor/progpilot/
-    mkdir ./vendor/progpilot/
-    mkdir ./vendor/progpilot/package
-    cp -R ../../package/* ./vendor/progpilot/package
-
-    rm -rf ../../builds/*
-        
-    echo "generating phar"
-
-    php -d phar.readonly=off ${composertool} build .
-    mv ./build.phar ../../builds/${newfile}.phar
-
-    rm composer.lock
-    rm -rf ./vendor/
-else
-
-echo ""
-echo "Error : ${composertool} doesn't exist"
-echo "download the latest release of phar composer tool : https://github.com/clue/phar-composer/releases"
-echo "and define the path of the tool in the composertool variable of this script"
-
+# Check if phar-composer.phar is in the $PATH
+if ! [ -x "$(command -v phar-composer.phar)" ]; then
+    echo "Error: phar-composer.phar command could not be found!"
+    echo "Make sure to download the latest release of phar composer tool: https://github.com/clue/phar-composer/releases"
+    echo "Make it executable and add it in a folder in your PATH"
+    exit 1
 fi
 
+version="dev"
+date=$(date "+%Y%m%d-%H%M%S")
+newfile="progpilot_${version}${date}"
+
+echo "Progpilot builder"
+echo "Did you update the version of progpilot in Console/Application.php file? (optional)"
+
+cd ./projects/phar || exit 1
+
+# Cleanup
+rm -f composer.lock
+rm -rf ./vendor/
+rm -rf ../../builds/*
+
+composer install
+rm -rf ./vendor/progpilot/
+mkdir -p ./vendor/progpilot/package
+cp -R ../../package/* ./vendor/progpilot/package
+
+echo "Generating phar..."
+php -d phar.readonly=off "$(command -v phar-composer.phar)" build .
+mv -v ./build.phar ../../builds/"${newfile}".phar
