ef473225165423bc63789e0511f721108da7710b
eric-therond <eric.therond.fr@gmail.com>
2023-01-09 20:28:10 +0000

    update version

--- package/src/progpilot/Console/Application.php
+++ package/src/progpilot/Console/Application.php
@@ -17,7 +17,7 @@ use progpilot\Lang;
 class Application extends BaseApplication
 {
     const NAME = 'progpilot';
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     public function __construct()
     {
